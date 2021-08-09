1489f9dd34f6d1c3a624d504fb7087ab78f1d4b1
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-09 16:20:28 -0400

    changes

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=37.8267,-122.4233&units=f';
 
 request({ url: url, json: true }, (error, response) => {
-   console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
+    if(error){
+        console.log("Unable to connect to weather service")
+    } else if (response.body.error) {
+        console.log("Unable to find location")
+    }
+    else {
+        console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`);
+    }
 })
 
 const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib2RhbmlzNzEiLCJhIjoiY2tzNTFrdXBsMTJ1ZzJ2bnRnYnR4NGFlZiJ9.kBfngGdrZ4v7jOOkBnUNZQ&limit=1"

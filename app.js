082ef4122e6067aafab269480bc0dd8132574b81
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-09 12:04:29 -0400

    weather description

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=37.8267,-122.4233&units=f';
 
 request({ url: url, json: true }, (error, response) => {
-   console.log(`It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
+   console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
 })
 

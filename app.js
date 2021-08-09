c23ec09b7be5fe9054cc1b00f0bbacb65cdeb817
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-09 11:45:25 -0400

    parsing the response

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=37.8267,-122.4233';
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.current)
+    console.log(response.body)
 })

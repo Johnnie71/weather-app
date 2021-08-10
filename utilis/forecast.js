30019fc79d87ffd42ef235dda1a7d92c38fc5e7b
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-10 11:10:23 -0400

    adjustments

--- utilis/forecast.js
+++ utilis/forecast.js
@@ -1,4 +1,5 @@
 const request = require('postman-request');
+
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=${latitude.toString()},${longitude.toString()}units=f`;
 

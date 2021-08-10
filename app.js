903b73f36b9ad4d06725b5935f698d57683d0cf7
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-10 10:52:54 -0400

    requiring forecast from file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('postman-request');
 const geoCode = require('./utilis/goecode');
+const forecast = require('./utilis/forecast');
 
 // const url = 'http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=37.8267,-122.4233&units=f';
 

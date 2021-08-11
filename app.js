2e2fa18982b534a779cc8fc7f102ab605ab6aa7b
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-11 19:41:28 -0400

    moved if statement for address

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const forecast = require('./utilis/forecast');
 
 const address = process.argv[2];
 
+//checking to see if an address was provided
+if(!address){
+    return console.log('Address needed!')
+}
+
 geoCode(address, (error, { latitude, longitude, location } = {}) => {
 //checking to see if there is an error
     if(error){
         return console.log(error)
-    }
-//checking to see if an address was provided
-    if(!address){
-        return console.log('Address needed!')
     }
 
  //if everything passes then the forecast is checked   

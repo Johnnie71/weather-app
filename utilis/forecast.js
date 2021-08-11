1e50e6da5f1f4fadbdf81be7d3c95347354922a2
johnnie71 <johnnie.agonz71@gmail.com>
2021-08-11 19:43:25 -0400

    destructuring response

--- utilis/forecast.js
+++ utilis/forecast.js
@@ -3,14 +3,14 @@ const request = require('postman-request');
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=3cd3eee99cacfba5d7e860d49be3016d&query=${latitude},${longitude}&units=f`;
    
-    request({ url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         
         if(error){
             callback('Unable to connect to weather service!', undefined)
-        } else if(response.body.error){
+        } else if(body.error){
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. There is a ${response.body.current.precip}% chance of rain.`)
         }
     })
 }
